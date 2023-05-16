292c12ade1ce0ca5895db340598eb5af87a465bb
León Cantillo <cleonjd17@gmail.com>
2023-05-16 06:36:33 -0500

    Creación de ventana Tienda

---
+++ html/tienda.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/nosotros.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Miscelanea ELLO</title>
+</head>
+<body>
+    <?php include 'header.html'?>
+    <section class="banner" style="--bg-banner: url(../img/jpg/seccion-comics.jpg)">
+        <h4 class="foco">Miscelanea ELLO</h1>
+        <img class="foco" src="../img/svg/logotipo-white.svg" alt="">
+    </section>
+    </body>
+</html>
