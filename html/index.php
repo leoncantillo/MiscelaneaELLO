16306c551d71b3c0019ffb933ba4a0ff67a78de4
León Cantillo <cleonjd17@gmail.com>
2023-05-16 06:33:35 -0500

    Cambio en el logotipo blanco. Creacion de ventana Nosotros. Implementación de código php, header general, banner general.

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Miscelanea ELLO</title>
+</head>
+<body>
+    <?php include 'header.html'?>
+    <div class="hero banner" style="--bg-banner: url(../img/jpg/Bg-Hero.jpg)">
+        <div class="foco">
+            <h2>Descuento en utiles escolares</h2>
+            <p>Encuentra las mejores ofertas<br/>Precios especiales</p>
+            <h4>¡No te lo pierdas!</h4>
+            <button class="sale-button">Ver artículos</button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ html/nosotros.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/nosotros.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Miscelanea ELLO</title>
+</head>
+<body>
+    <?php include 'header.html'?>
+    <section class="banner" style="--bg-banner: url(../img/jpg/bg-desenfocado.webp)">
+        <h4 class="foco">Miscelanea ELLO</h1>
+        <img class="foco" src="../img/svg/logotipo-white.svg" alt="">
+    </section>
+    <section>
+        <div class="contenedor dos-parrafos">
+            <div class="caja-parrafo">
+                <h3>¿Quienes Somos?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla tenetur quis ratione voluptatem reiciendis corporis veniam, maiores culpa hic voluptatum cumque possimus temporibus numquam harum odio quo, quasi alias. Et voluptatum eligendi id vel facere eius maiores minus commodi. Natus laudantium velit nam amet, quam dolores aut facere, quibusdam odit eaque cumque! Corporis est quas, facilis dolorum, nostrum necessitatibus recusandae dolor blanditiis nihil, dolore aut dignissimos praesentium unde eos fuga?</p>
+            </div>
+            <div>
+                <img class="burbuja" src="../img/jpg/img-placeholder.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="bg-mision">
+        <div class="contenedor dos-parrafos">
+            <div>
+                <img class="burbuja" src="../img/jpg/img-placeholder.jpg" alt="">
+            </div>
+            <div class="caja-parrafo seccion-mision">
+                <h3>Misión</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla tenetur quis ratione voluptatem reiciendis corporis veniam, maiores culpa hic voluptatum cumque possimus temporibus numquam harum odio quo, quasi alias. Et voluptatum eligendi id vel facere eius maiores minus commodi. Natus laudantium velit nam amet, quam dolores aut facere, quibusdam odit eaque cumque! Corporis est quas, facilis dolorum, nostrum necessitatibus recusandae dolor blanditiis nihil, dolore aut dignissimos praesentium unde eos fuga?</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="contenedor dos-parrafos">
+            <div class="caja-parrafo">
+                <h3>Visión</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla tenetur quis ratione voluptatem reiciendis corporis veniam, maiores culpa hic voluptatum cumque possimus temporibus numquam harum odio quo, quasi alias. Et voluptatum eligendi id vel facere eius maiores minus commodi. Natus laudantium velit nam amet, quam dolores aut facere, quibusdam odit eaque cumque! Corporis est quas, facilis dolorum, nostrum necessitatibus recusandae dolor blanditiis nihil, dolore aut dignissimos praesentium unde eos fuga?</p>
+            </div>
+            <div>
+                <img class="burbuja" src="../img/jpg/img-placeholder.jpg" alt="">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
