7bc33e1be4f18740e00504645fe25bb09c20aafa
León Cantillo <cleonjd17@gmail.com>
2023-05-16 22:28:44 -0500

    pagina tienda, estilos y estructura

--- html/tienda.php
+++ html/tienda.php
@@ -4,16 +4,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/nosotros.css">
-    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/tienda.css">
     <title>Miscelanea ELLO</title>
 </head>
 <body>
     <?php include 'header.html'?>
-    <section class="banner" style="--bg-banner: url(../img/jpg/seccion-comics.jpg)">
+    <!-- <section class="banner" style="--bg-banner: url(../img/jpg/seccion-comics.jpg)">
         <h4 class="foco">Miscelanea ELLO</h1>
         <img class="foco" src="../img/svg/logotipo-white.svg" alt="">
+    </section> -->
+    <section class="tienda-home">
+        <aside>
+            <h4>Filtros</h4>
+            <ul>
+                <li>Etiquetas</li>
+                <li>Categorías</li>
+                <li>Ubicación</li>
+                <li>Condición
+                    <ul>
+                        <li>Nuevo</li>
+                        <li>Usado</li>
+                    </ul>
+                </li>
+            </ul>
+        </aside>
+        <section>
+            <div class="ordenar-productos">Ordenar por <span>Más Vendidos</span></div>
+            <div class="productos-en-venta">
+                <div class="producto-en-vitrina">
+                    <div class="imagen-producto">
+                        <img src="../img/jpg/Productos/Cuaderno anillado.jpg" alt="">
+                    </div>
+                    <div class="descripcion-del-producto">
+                        <h3>Cuaderno Anillado 5 Materias</h3>
+                        <span>$ 8000.00 cop</span> <!--Este valor será dinámico -->
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam iure sit est fugit magni earum enim et inventore quia dolores?</p>
+                        <div>
+                            <span>Etiquetas: Taltal</span>
+                            <span>Categoría: Blabla bla</span>
+                            <span>Color: Rojo</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="producto-en-vitrina">
+                    <div class="imagen-producto">
+                        <img src="../img/jpg/Productos/Caja Lapiz HB.jpg" alt="">
+                    </div>
+                    <div class="descripcion-del-producto">
+                        <h3>Caja De Lapices HB Faber Castell</h3>
+                        <span>$ 4800.00 cop</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam iure sit est fugit magni earum enim et inventore quia dolores?</p>
+                        <div>
+                            <span>Etiquetas: Taltal</span>
+                            <span>Categoría: Blabla bla</span>
+                            <span>Color: Rojo</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </section>
-    </body>
+</body>
 </html>
