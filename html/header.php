44d36e6357ad44698348f094000e984044c9c8a5
León Cantillo <cleonjd17@gmail.com>
2023-05-21 11:57:23 -0500

    dinamizando el boton para las opciones de usuario

--- html/header.php
+++ html/header.php
@@ -1,6 +1,3 @@
-<link rel="stylesheet" href="../css/header.css">
-<link rel="stylesheet" href="../css/reset.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <header>
     <div class="header">
         <div class="header__container">
@@ -24,30 +21,31 @@
                 <span class="header__searcher--placeholder">Buscar</span>
             </div>
             <div class="header__nav--useroptions">
-                <div class="header__nav--useroptions--button">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 71.04 76.96"><defs></defs>
-                        <g id="Capa_2" data-name="Capa 2">
-                        <g id="Layer_1" data-name="Layer 1">
-                        <path class="cls-1" d="M47.76,40.7a22.2,22.2,0,1,0-24.48,0A35.53,35.53,0,0,0,0,74v3H71V74A35.53,35.53,0,0,0,47.76,40.7ZM19.24,22.2A16.28,16.28,0,1,1,35.52,38.48,16.29,16.29,0,0,1,19.24,22.2ZM6.07,71A29.6,29.6,0,0,1,65,71Z"/>
-                        </g></g>
-                    </svg>
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
-                    </svg>
-                </div>
+                <input type="checkbox" name="" id="active-useroptions">
+                <label class="header__nav--useroptions--button">
+                    <i class="fa-regular fa-user"></i>
+                    <i class="fa-solid fa-chevron-down"></i>
+                </label>
                 <!-- menu desplegable -->
                 <div class="menu-opciones-usuario">
                     <?php
                         include 'user-options-menu-header.php';
 
                         $logueado = true; // Esto cambia según la lógica de la aplicación
-                        $esAdmin = false; // Esto cambia según la lógica de la aplicación
+                        $esAdmin = true; // Esto cambia según la lógica de la aplicación
 
                         // Llamar a la función para generar e imprimir la lista de opciones
                         generarListaOpciones($logueado, $esAdmin);
                     ?>
-
                 </div>
+                <script>
+                    const checkbox = document.getElementById("active-useroptions");
+                    const label = document.querySelector(".header__nav--useroptions--button");
+
+                    label.addEventListener("click", function() {
+                        checkbox.checked = !checkbox.checked;
+                    });
+                </script>
             </div>
         </div>
     </div>
