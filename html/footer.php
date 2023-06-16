aa439428bae0ed19855198ceba79188d5cb12a26
León Cantillo <cleonjd17@gmail.com>
2023-06-16 12:52:42 -0500

    archivo de footer individual

---
+++ html/footer.php
@@ -0,0 +1,33 @@
+<footer>
+        <div class="contfooter form">
+            <div class="footerbox">
+                <h4>CONTACTO</h4>
+                <form action="">
+                    <input type="text" name="" id="" placeholder="Escribe tu mensaje...">
+                    <input type="submit" value="ENVIAR">
+                </form>
+            </div>
+        </div>
+        <div class="contfooter info">
+            <div class="footerbox">
+                <img class="logo-footer" src="../img/svg/logotipo-white.svg" alt="logotipo-white">
+                <ul class="links">
+                    <li>INICIO</li>
+                    <li>TIENDA</li>
+                    <li>NOSOTROS</li>
+                    <li>CONTACTO</li>
+                </ul>
+                <div class="networks">
+                    <span>f</span>
+                    <span>I</span>
+                </div>
+                <ul class="contact">
+                    <li>000-000-000-00</li>
+                    <li>DIRECCIÓN #00-00</li>
+                    <li>CORREO@GMAIL.COM</li>
+                    <li>BARRANQUILLA COL.</li>
+                </ul>
+                <img class="qr-code" src="../sources/codigo.png" alt="">
+            </div>
+        </div>
+</footer>
