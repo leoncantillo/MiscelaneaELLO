771390af264cbb326b27113b32eeb77a1c253036
León Cantillo <cleonjd17@gmail.com>
2023-06-16 11:38:51 -0500

    Footer agregado, corrección en implementación del MVC

--- html/index.php
+++ html/index.php
@@ -6,19 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/home.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <title>Miscelanea ELLO</title>
+    <link rel="stylesheet" href="../css/header-footer.css">
 </head>
 <body>
+    <!--HEADER-->
     <?php include 'header.php'?>
-    <div class="hero banner" style="--bg-banner: url(../img/jpg/Bg-Hero.jpg)">
-        <div class="foco">
-            <h2>Descuento en utiles escolares</h2>
-            <p>Encuentra las mejores ofertas<br/>Precios especiales</p>
-            <h4>¡No te lo pierdas!</h4>
-            <button class="sale-button">Ver artículos</button>
+    
+    <?php
+        include 'home.php'
+    ?>
+
+    <!--FOOTER-->
+    <footer>
+        <div class="contfooter form">
+            <div class="footerbox">
+                <h4>CONTACTO</h4>
+                <form action="">
+                    <input type="text" name="" id="" placeholder="Escribe tu mensaje...">
+                    <input type="submit" value="ENVIAR">
+                </form>
+            </div>
         </div>
-    </div>
+        <div class="contfooter info">
+            <div class="footerbox">
+                <img class="logo-footer" src="../img/svg/logotipo-white.svg" alt="logotipo-white">
+                <ul class="links">
+                    <li>INICIO</li>
+                    <li>TIENDA</li>
+                    <li>NOSOTROS</li>
+                    <li>CONTACTO</li>
+                </ul>
+                <div class="networks">
+                    <span>f</span>
+                    <span>I</span>
+                </div>
+                <ul class="contact">
+                    <li>000-000-000-00</li>
+                    <li>DIRECCIÓN #00-00</li>
+                    <li>CORREO@GMAIL.COM</li>
+                    <li>BARRANQUILLA COL.</li>
+                </ul>
+                <img class="qr-code" src="../sources/codigo.png" alt="">
+            </div>
+        </div>
+</footer>
 </body>
 </html>
