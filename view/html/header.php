bdadf1e5f916c367601802d67adc01c820506e17
León Cantillo <cleonjd17@gmail.com>
2023-06-19 21:29:45 -0500

    Empezando a implementar header responsive, adición del icono de meú hamburguesa

--- view/html/header.php
+++ view/html/header.php
@@ -7,7 +7,6 @@
             </figure>
             <nav class="header__nav--main">
                 <ul>
-                    
                     <?php if(isset($_GET["rute"])): ?>
                         <!-- HOME -->
                         <?php if($_GET["rute"] == "home"): ?>
@@ -45,8 +44,10 @@
                         <li><a href="index.php?rute=contact">Contacto</a></li>
 
                     <?php endif ?>
-                    
                 </ul>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #00000;transform: ;msFilter:;">
+                    <path d="M4 6h16v2H4zm4 5h12v2H8zm5 5h7v2h-7z"></path>
+                </svg>
             </nav>
             <div class="header__searcher">
                 <!-- cambiar por un input -->
