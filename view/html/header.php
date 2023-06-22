f1324c3de6786283eb68e798ffd8070cadb0ce64
León Cantillo <cleonjd17@gmail.com>
2023-06-22 13:18:46 -0500

    Reemplazando el boceto del buscador por un input. Aún no tiene funcionalidad con la base de datos. Además se realizó un estilizado responsivo por debajo de los 600px. Queda pendiente el estilizado del buscador para la pantalla menor de 600px

--- view/html/header.php
+++ view/html/header.php
@@ -6,9 +6,9 @@
                 <h1>ELLO</h1>
             </figure>
             <nav class="header__nav--main">
-                <span class="show-menu"><i class="fa-solid fa-bars">H</i></span>
+                <span class="show-menu"><i class="fa-solid fa-bars"></i></span>
                 <div class="header__nav--list closed">
-                    <span class="hide-menu"><i class="fa-regular fa-circle-xmark">X</i></span>
+                    <span class="hide-menu"><i class="fa-regular fa-circle-xmark"></i></span>
                     <ul>
                         <?php if(isset($_GET["rute"])): ?>
                             <!-- HOME -->
@@ -64,11 +64,11 @@
                 </script>
             </nav>
             <div class="header__searcher">
-                <!-- cambiar por un input -->
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-                    <path d="M17.545 15.467l-3.779-3.779c0.57-0.935 0.898-2.035 0.898-3.21 0-3.417-2.961-6.377-6.378-6.377s-6.186 2.769-6.186 6.186c0 3.416 2.961 6.377 6.377 6.377 1.137 0 2.2-0.309 3.115-0.844l3.799 3.801c0.372 0.371 0.975 0.371 1.346 0l0.943-0.943c0.371-0.371 0.236-0.84-0.135-1.211zM4.004 8.287c0-2.366 1.917-4.283 4.282-4.283s4.474 2.107 4.474 4.474c0 2.365-1.918 4.283-4.283 4.283s-4.473-2.109-4.473-4.474z"></path>
-                </svg>                    
-                <span class="header__searcher--placeholder">Buscar</span>
+                <input class="header__searcher--input" type="text" required/>
+                <div class="header__searcher--placeholder">
+                    <i class="fa-solid fa-magnifying-glass"></i>                   
+                    <span>Buscar</span>
+                </div>
             </div>
             <div class="header__nav--useroptions">
                 <input class="header__nav--useroptions--select" type="checkbox" name="" id="useroptions--select">
