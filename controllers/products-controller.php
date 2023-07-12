c96123cf2759df1df38c05a52bd9353938f5917b
León Cantillo <cleonjd17@gmail.com>
2023-07-12 17:44:18 -0500

    Metodo para actualizar los archivos de una tabla, terminado y corregido. Atualización de los nombres de los métodos que fueron movidos de posición. Implementación de nuevas validaciones,

--- controllers/products-controller.php
+++ controllers/products-controller.php
@@ -18,30 +18,18 @@ Class ProductsController {
                     $conditionValue = 1;
             }
 
-            $data = array("product_name" => GlobalControllers::test_input($_POST["product-name"]),
-                          "description" => GlobalControllers::test_input($_POST["description"]),
-                          "image" => GlobalControllers::upload_file($postFile, $destinationFolder),
-                          "price" => GlobalControllers::test_input($_POST["price"]),
-                          "promotion_price" => GlobalControllers::test_input($_POST["promotion-price"]),
-                          "tag_id" => GlobalControllers::test_input($_POST["product-tag"]),
-                          "category_id" => GlobalControllers::test_input($_POST["product-category"]),
-                          "color" => GlobalControllers::test_input($_POST["product-color"]),
-                          "condition" => GlobalControllers::test_input($conditionValue));
+            $data = array("product_name" => GlobalController::test_input($_POST["product-name"]),
+                          "description" => GlobalController::test_input($_POST["description"]),
+                          "image" => GlobalController::upload_file($postFile, $destinationFolder),
+                          "price" => GlobalController::test_input($_POST["price"]),
+                          "promotion_price" => GlobalController::test_input($_POST["promotion-price"]),
+                          "tag_id" => GlobalController::test_input($_POST["product-tag"]),
+                          "category_id" => GlobalController::test_input($_POST["product-category"]),
+                          "color" => GlobalController::test_input($_POST["product-color"]),
+                          "condition" => GlobalController::test_input($conditionValue));
             
             $answer = GlobalModel::mdlInsertData($table, $data);
             return $answer;
-        }
-    }
-
-    # DELETE ----------------------------------
-    static public function ctrDeleteProducts() {
-        if ($_SERVER["REQUEST_METHOD"] == "GET") {
-            if (isset($_GET["id"])) {
-                $table = "ellodb_products";
-                $idProduct = GlobalControllers::test_input($_GET["id"]);
-                $answer = GlobalModel::mdlDeleteData($table, $idProduct);
-                return $answer;
-            }
         }
     }
 
@@ -49,6 +37,14 @@ Class ProductsController {
     static public function ctrSelectProducts() {
         $table = "ellodb_products";
         $answer = GlobalModel::mdlFetchData($table, null, null);
+        return $answer;
+    }
+
+    static public function ctrSelectProductsWithId($id) {
+        $table = "ellodb_products";
+        $columnName = "id";
+        $idFetch = GlobalController::test_input($id);
+        $answer = GlobalModel::mdlFetchData($table, $columnName, $idFetch);
         return $answer;
     }
 
@@ -61,6 +57,57 @@ Class ProductsController {
     static public function ctrSelectCategories() {
         $table = "ellodb_categories";
         $answer = GlobalModel::mdlFetchData($table, null, null);
+        return $answer;
+    }
+
+    # UPDATE ----------------------------------
+    static public function ctrUpdateProduct() {
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $table = "ellodb_products";
+            
+            $promotionPrice = GlobalController::test_input($_POST['promotion-price']);
+            if (empty($promotionPrice) || floatval($promotionPrice) === 0) {
+                $promotionPrice = null;
+            }
+
+            $conditionValue = 0;
+            if(isset($_POST["condition"])){
+                if($_POST["condition"] == "product-new")
+                    $conditionValue = 1;
+            }
+
+            $data = array("id" => GlobalController::test_input($_POST["product-id"]),
+                          "product_name" => GlobalController::test_input($_POST["product-name"]),
+                          "description" => GlobalController::test_input($_POST["description"]),
+                          "price" => GlobalController::test_input($_POST["price"]),
+                          "promotion_price" => $promotionPrice,
+                          "tag_id" => GlobalController::test_input($_POST["product-tag"]),
+                          "category_id" => GlobalController::test_input($_POST["product-category"]),
+                          "color" => GlobalController::test_input($_POST["product-color"]),
+                          "condition" => GlobalController::test_input($conditionValue));
+
+            # se empieza desde la carpeta /controllers
+            $destinationFolder = "/../view/img/products/";
+
+            if (!empty($_POST["product-image-temp"])) {
+                $data["image"] = GlobalController::test_input($_POST["product-image-temp"]);
+            }
+
+            if (!empty($_FILES["product-image"]["name"])) {
+                $postFile = $_FILES["product-image"];
+                $data["image"] = GlobalController::upload_file($postFile, $destinationFolder);
+            }
+            
+            $answer = GlobalModel::mdlUpdateData($table, $data);
+            return $answer;
+        }
+    }
+
+    # DELETE ----------------------------------
+    static public function ctrDeleteProducts($id) {
+        $table = "ellodb_products";
+        $idProduct = GlobalController::test_input($id);
+        $answer = GlobalModel::mdlDeleteData($table, $idProduct);
         return $answer;
     }
 }
