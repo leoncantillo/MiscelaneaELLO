9d8e25830688da0b4484e1aedaca47d4e46091c4
León Cantillo <cleonjd17@gmail.com>
2023-07-12 17:49:28 -0500

    atrapando el valor del id desde el archivo para eliminar, con el fin de solo mandar el valor a travéz de la variable . Ademas actualización de la llamada al controlador para eliminar.

--- view/html/delete-product.php
+++ view/html/delete-product.php
@@ -1,16 +1,21 @@
 <?php
 
-$deleteProduct = FormsController::ctrDeleteProducts();
-if ($deleteProduct) {
-    echo "<script>
-        alert('El producto ha sido eliminado.');
-        window.location = 'index.php?rute=manage';
-    </script>";
-} else {
-    echo "<script>
-        alert('Hubo un error, intentelo nuevamente');
-        window.location = 'index.php?rute=manage';
-    </script>";
-}
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+        $deleteProduct = ProductsController::ctrDeleteProducts($id);
 
+        if ($deleteProduct) {
+            echo "<script>
+                alert('El producto ha sido eliminado.');
+                window.location = 'index.php?rute=manage';
+            </script>";
+        } else {
+            echo "<script>
+                alert('Hubo un error, intentelo nuevamente');
+                window.location = 'index.php?rute=manage';
+            </script>";
+        }
+    }
+}
 ?>
